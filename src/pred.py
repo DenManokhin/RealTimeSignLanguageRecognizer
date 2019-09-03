10c0cf9812930ee961f15224d87c562375c3b793
den <denny1301@gmail.com>
2019-09-03 17:29:16 +0300

    Performance improved

--- src/pred.py
+++ src/pred.py
@@ -10,7 +10,10 @@ def predict(model:CNN, image:np.ndarray, device:torch.device):
     tensor_norm = image_tensor / 255
     input = tensor_norm.view((1, 1, 28, 28))
 
-    model.to(device, torch.float)
-    outputs = model(input)
-    _, pred = outputs.max(1)
+    with torch.no_grad():
+        model.eval()
+        model.to(device, torch.float)
+        outputs = model(input)
+        _, pred = outputs.max(1)
+
     return pred
