89b84e7520c4d909a6586ef47c00b49e705870eb
den <denny1301@gmail.com>
2019-09-04 12:20:43 +0300

    Gesture classifier now accepts grayscale images instead of masks

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from model import CNN
 
 # load model parameters and copy it to GPU if available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = torch.load("../weights.pth", map_location=device).to(device, torch.float)
+model = torch.load("../model_color_8.pth", map_location=device).to(device, torch.float)
 
 
 calibrated = False
@@ -43,35 +43,19 @@ while cap.isOpened():
 
         if calibrated:
 
-            # select hand roi and apply mask by skin color
+            # select hand roi and convert to grayscale
             hand_roi = frame_blur[y:y + h, x:x + w].copy()
-            roi_ycrcb = cv.cvtColor(hand_roi, cv.COLOR_BGR2YCrCb)
-            lower = np.array([60, max(0, cr - cr_diff), max(0, cb - cb_diff)], dtype="uint8")
-            upper = np.array([255, min(255, cr + cr_diff), min(255, cb + cb_diff)], dtype="uint8")
-            mask = cv.inRange(roi_ycrcb, lower, upper)
-
-            # apply morphology operations to improve mask quality
-            kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (7, 7))
-            mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, kernel)
-            mask = cv.dilate(mask, kernel)
+            hand_roi_gray = cv.cvtColor(hand_roi, cv.COLOR_BGR2GRAY)
 
             # make prediction on gesture
-            pred = predict(model, mask, device)
+            pred = predict(model, hand_roi_gray, device)
             char = class_map[pred.item()]
             cv.putText(frame_resized, "Char: " + char, (20, 430), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
 
         else:
 
-            # set color of central pixel of hand roi as skin color
             if cv.waitKey(1) & 0xFF == ord(" "):
-                hand_roi = frame_blur[y:y + h, x:x + w].copy()
-                roi_ycrcb = cv.cvtColor(hand_roi, cv.COLOR_BGR2YCrCb)
-                _, cr, cb = roi_ycrcb[h // 2, w // 2, :]
                 calibrated = True
-
-            # mark hint where to place a hand
-            cv.circle(frame_resized, (x + w // 2, y + h // 2), 3, (0, 255, 0), 3)
-            mask = np.zeros((h, w))
 
         # fps counter
         end = time()
@@ -81,7 +65,6 @@ while cap.isOpened():
         # display frame
         cv.rectangle(frame_resized, (x, y), (x+w, y+h), (0, 255, 0), 2)
         cv.imshow("Gesture Recognizer", frame_resized)
-        cv.imshow("Mask", mask)
 
         # exit if 'q' is pressed
         if cv.waitKey(1) & 0xFF == ord("q"):
